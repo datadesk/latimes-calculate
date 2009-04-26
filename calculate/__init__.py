5a0b95b4568e0c1a0565626daea78b53f242d242
palewire <ben.welsh@latimes.com>
2009-04-26 14:58:46 -0700

    Reorged against to tidy things further. This way I can keep the functions in distinct files, which I prefer

--- calculate/__init__.py
+++ calculate/__init__.py
@@ -1,1 +1,7 @@
+from decile import decile
 from elfi import elfi
+from ordinal_rank import ordinal_rank
+from per_capita import per_capita
+from per_sqmi import per_sqmi
+from percent_change import percent_change
+from percentage import percentage
