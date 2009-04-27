f64ba7d30ff9b46de879277e9be6cd11b6cb3c03
palewire <ben.welsh@latimes.com>
2009-04-27 00:39:20 -0700

    Added random_point utility

--- calculate/__init__.py
+++ calculate/__init__.py
@@ -7,5 +7,6 @@ from per_capita import per_capita
 from per_sqmi import per_sqmi
 from percentage_change import percentage_change
 from percentage import percentage
+from random_point import random_point
 from standard_deviation_distance import standard_deviation_distance
 from standard_deviation_ellipses import standard_deviation_ellipses
