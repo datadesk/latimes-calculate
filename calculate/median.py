1a44101d54c5a57b59c26d8100768475e22d5572
palewire <ben.welsh@latimes.com>
2009-05-28 20:22:52 -0700

    Fixed order of operation fudgup in the media function

--- calculate/median.py
+++ calculate/median.py
@@ -5,6 +5,7 @@ def median(data_list):
 	data_list = map(float, data_list)
 	n = len(data_list)
 	data_list.sort()
+	print data_list
 	# Test whether the n is odd
 	if n & 1:
 		# If is is, get the index simply by dividing it in half
@@ -12,7 +13,10 @@ def median(data_list):
 		return data_list[index]
 	else:
 		# If the n is even, average the two values at the center
+		print "EVEN!"
 		low_index = n / 2 - 1
+		print low_index
 		high_index = n / 2
-		average = data_list[low_index] + data_list[high_index] / 2
+		print high_index
+		average = (data_list[low_index] + data_list[high_index]) / 2
 		return average
