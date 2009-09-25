415d6d42fde01dd8ab0f5ce963f32006054d4df6
Ben Welsh <benwelsh@tuohy.local>
2009-09-25 16:58:40 -0700

    Updated docstring

--- calculate/benfords_law.py
+++ calculate/benfords_law.py
@@ -5,7 +5,7 @@ def benfords_law(number_list, method='last_digit'):
 	Accepts a list of numbers and applys a quick-and-dirty run against Benford's Law.
 	
 	This function is incomplete.
-	
+		
 	h3. Example usage
 	
 		>> import calculate
@@ -24,10 +24,41 @@ def benfords_law(number_list, method='last_digit'):
 		| 8      | 1     | 0.1        |
 		| 9      | 1     | 0.1        |
 	
+		h3. A Warning
+
+		Not all datasets should be expected to conform these rules.
+
+		Durtschi, Hillison, and Pacini (2004) said Benford "compliance"
+		should be expected in the following circumstances:
+
+			1. Numbers that result from mathematical combination of 
+			numbers (e.g., quantity  × price)
+
+			2. Transaction-level data (e.g., disbursements, sales) 
+
+			3. Large datasets 
+
+			4. Mean is greater than median and skew is positive 
+
+		And not to expect Benford distributions when:
+
+			1. Numbers are assigned (e.g., check numbers, invoice numbers) 
+
+			2. Numbers inﬂuenced by human thought (e.g., prices set by 
+			psychological thresholds ($1.99)) 
+
+			3. Accounts with a large number of ﬁrm-speciﬁc numbers 
+			(e.g., accounts set up to record $100 refunds) 
+
+			4. Accounts with a built-in minimum or maximum 
+
+			5. Where no transaction is recorded.
+	
 	h3. Sources
 	
 		"Benford's Law":http://en.wikipedia.org/wiki/Benford%27s_law
 		"Applying Benford's Law to CAR":http://www.chasedavis.com/2008/sep/28/applying-benfords-law-car/
+		"Breaking the (Benford) Law: Statistical Fraud Detection in Campaign Finance (pdf)":http://cho.pol.uiuc.edu/wendy/papers/tas.pdf
 		"Benford's Law meets Python and Apple Stock Prices":http://pyevolve.sourceforge.net/wordpress/?p=457
 		"Strategic Vision Polls Exhibit Unusual Patterns, Possibly Indicating Fraud":http://www.fivethirtyeight.com/2009/09/strategic-vision-polls-exhibit-unusual.html
 		"Nate Silver: pollster may be fraud":http://blogs.tampabay.com/buzz/2009/09/nate-silver-pollster-may-be-fraud.html
