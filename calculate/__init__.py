5fcc8c436c89ab468eeb556764951d40fe35bd14
Ben Welsh <benwelsh@tuohy.local>
2009-09-27 22:21:56 -0700

    Redrafted the __init__ imports to make things more inline with Bycoffe's model, which allows for use outside of Django. The new HAS_DJANGO variable lets you know what's up

--- calculate/__init__.py
+++ calculate/__init__.py
@@ -1,15 +1,11 @@
 from adjusted_monthly_value import adjusted_monthly_value
 from benfords_law import benfords_law
-from competition_rank import competition_rank
 from date_range import date_range
 from decile import decile
 from elfi import elfi
-from mean_center import mean_center
 from mean import mean
 from median import median
 from mode import mode
-from nudge_points import nudge_points
-from ordinal_rank import ordinal_rank
 from per_capita import per_capita
 from per_sqmi import per_sqmi
 from percentage_change import percentage_change
@@ -18,6 +14,21 @@ from percentile import percentile
 from random_point import random_point
 from range import range
 from standard_deviation import standard_deviation
-from standard_deviation_distance import standard_deviation_distance
-from standard_deviation_ellipses import standard_deviation_ellipses
 from summary_stats import summary_stats
+
+# Test whether Django is installed
+from django.conf import settings
+if settings.configured:
+	HAS_DJANGO = True
+else:
+	HAS_DJANGO = False
+
+# If it is, import all the functions that, at least for now, require Django
+if HAS_DJANGO:
+	from ordinal_rank import ordinal_rank
+	from competition_rank import competition_rank
+	from random_point import random_point
+	from mean_center import mean_center
+	from nudge_points import nudge_points
+	from standard_deviation_distance import standard_deviation_distance
+	from standard_deviation_ellipses import standard_deviation_ellipses
