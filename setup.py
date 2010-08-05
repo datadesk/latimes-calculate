1901648d7d542bf0ddeca6f06d0c5f25961b0b89
palewire <ben.welsh@gmail.com>
2010-08-05 01:10:15 -0700

    Added a setup.py and pushed to pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='calculate',
+      version='0.1',
+      description='A collection of quickie math functions. Nothing too fancy.',
+      author='Ben Welsh',
+      author_email='Benjamin.Welsh@latimes.com',
+      url='http://github.com/datadesk/latimes-calculate',
+      download_url='http://github.com/datadesk/latimes-calculate.git',
+      packages=find_packages(),
+      license='MIT',
+      keywords='geocode geocoding gis geographical maps earth distance',
+      classifiers=["Intended Audience :: Developers",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries :: Python Modules"
+                   ],
+     )
